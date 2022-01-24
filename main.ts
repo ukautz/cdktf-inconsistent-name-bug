d44798d1f64bf595f2019e6a74fccd6b52871792
Ulrich Kautz <mail@ulrichkautz.com>
2022-01-24 21:46:03 +0000

    Use provider 1, create some resources

--- main.ts
+++ main.ts
@@ -1,14 +1,24 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import * as hcloud from "./.gen/providers/hcloud";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
+    new hcloud.HcloudProvider(this, "CloudProvider", {
+      token: "the-token",
+    });
+
+    new hcloud.SshKey(this, "HetznerSshKey", {
+      name: "My Key",
+      publicKey: "ecdsa-sha2-nistp521 AAAAE..",
+    });
+
+    // define more resources here ...
   }
 }
 
 const app = new App();
-new MyStack(app, "terraform-cdk-generate-bug");
+new MyStack(app, "inconsistent-name-bug");
 app.synth();

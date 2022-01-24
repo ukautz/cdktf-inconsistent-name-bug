97f3a04885d6c76adf04c84ddc45777ca69f6e35
Ulrich Kautz <mail@ulrichkautz.com>
2022-01-24 21:49:30 +0000

    Add provider 2, having resources of provider 1 renamed

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import * as digitalocean from "./.gen/providers/digitalocean";
 import * as hcloud from "./.gen/providers/hcloud";
 
 class MyStack extends TerraformStack {
@@ -10,8 +11,18 @@ class MyStack extends TerraformStack {
       token: "the-token",
     });
 
-    new hcloud.SshKey(this, "HetznerSshKey", {
+    // OOPS: SshKey became SshKeyA??
+    new hcloud.SshKeyA(this, "HetznerSshKey", {
       name: "My Key",
+      publicKey: "ecdsa-sha2-nistp521 AAAAE..",
+    });
+
+    new digitalocean.DigitaloceanProvider(this, "CloudProvider", {
+      token: "the-token",
+    });
+
+    new digitalocean.SshKey(this, "SshKey", {
+      name: `My Key`,
       publicKey: "ecdsa-sha2-nistp521 AAAAE..",
     });
 
